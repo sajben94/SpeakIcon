3b95fc8ecc2ae9a9c23b5ea1086cbb8c7d23c2ed
sajben94 <sajben94@gmail.com>
2016-04-26 16:39:24 +0200

    Swipe delete from dock

--- js/index.js
+++ js/index.js
@@ -6,7 +6,10 @@
     document.addEventListener('resume', onResume,false);
     $('.icon-wrapper').load('template/menu.html');
 
-    $(document).on('touchstart','.menu-icon',function () {
+
+
+
+    $(document).on('touchend','.menu-icon',function () {
       // alert('hele');
     $('.icon-wrapper').load('template/'+ $(this).attr('id')+'.html');
 
@@ -20,12 +23,23 @@
 
     var addID=1;
     $('#1').css('border', '2px solid red');
+    // $('#1').css('border', '2px solid red');
     $('#1').css('border', '2px solid transparent');
 
     $(document).on('touchstart','.dlist',function () {
       addID=$(this).attr('id');
       $('.dlist').css('border', '2px solid transparent');
       $(this).css('border', '2px solid red');
+
+      //swipe start
+          var myElement = document.getElementById(addID);
+          var mc = new Hammer(myElement);
+          mc.get('pan').set({ direction: Hammer.DIRECTION_ALL });
+          mc.on("panup", function(ev) {
+            $('#'+addID).html('<i class="demo-icon icon-cancel">&#xe846;</i>');
+          });
+      //swipe end
+
     });
 
 
@@ -36,25 +50,12 @@
           $('.dlist').css('border', '2px solid transparent');
           $('#'+addID).css('border', '2px solid red');
         }
+        else{
+          addID=1;
+          $('.dlist').css('border', '2px solid transparent');
+          $('#'+addID).css('border', '2px solid red');
+        }
     });
-
-
-
-
-
-    //Keep track of how many swipes
-    var count=0;
-    //Enable swiping...
-    $(document).swipe( {
-      //Single swipe handler for left swipes
-      swipeLeft:function(event, direction, distance, duration, fingerCount) {
-        $(this).text("You swiped " + direction + " " + ++count + " times " );
-      },
-      //Default is 75px, set to 0 for demo so any distance triggers swipe
-      threshold:0
-    });
-
-
   }
 
   function onPause(){

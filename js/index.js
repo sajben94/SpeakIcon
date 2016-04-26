b8341de6571984bc2c0604a91201daea25fc8e40
sajben94 <sajben94@gmail.com>
2016-04-26 19:20:56 +0200

    Zoom dock icon

--- js/index.js
+++ js/index.js
@@ -4,56 +4,70 @@
   function onDeviceReady() {
     document.addEventListener('pause', onPause,false);
     document.addEventListener('resume', onResume,false);
+    // Pridavanie menu ikoniek
     $('.icon-wrapper').load('template/menu.html');
 
 
 
 
     $(document).on('touchend','.menu-icon',function () {
-      // alert('hele');
-    $('.icon-wrapper').load('template/'+ $(this).attr('id')+'.html');
+      // Pridavanie ikoniek s kategorii
+      $('.icon-wrapper').load('template/'+ $(this).attr('id')+'.html');
 
-    if($(this).attr('id')=="menu"){
-      $('.dlist').css('border', '2px solid transparent');
-    }else{
-      $('#'+addID).css('border', '2px solid red');
-    }
-
+      //Pohybovanie cerveneho kruhu
+      if($(this).attr('id')=="menu"){
+        $('.dlist').css('border', '2px solid transparent');
+        $('.icon-line').show();
+      }else{
+        $('#'+addID).css('border', '2px solid white');
+      }
     });
 
+      //Viznacovanie cerveneho kruhu
     var addID=1;
-    $('#1').css('border', '2px solid red');
-    // $('#1').css('border', '2px solid red');
+    $('#1').css('border', '2px solid white');
     $('#1').css('border', '2px solid transparent');
 
-    $(document).on('touchstart','.dlist',function () {
+    //Editovanie dock ikoniek
+    $(document).on('touchend','.dlist',function () {
       addID=$(this).attr('id');
       $('.dlist').css('border', '2px solid transparent');
-      $(this).css('border', '2px solid red');
+      $(this).css('border', '2px solid white');
 
       //swipe start
-          var myElement = document.getElementById(addID);
-          var mc = new Hammer(myElement);
+      //Mazanie ikoniek
+          var mc = new Hammer(this);
           mc.get('pan').set({ direction: Hammer.DIRECTION_ALL });
           mc.on("panup", function(ev) {
             $('#'+addID).html('<i class="demo-icon icon-cancel">&#xe846;</i>');
+
+          });
+          mc.on("press", function(ev) {
+            // $('#'+addID).html('<i class="demo-icon icon-cancel">&#xe846;</i>');
+            if (!$('.list').find('i').hasClass('class-cancel')) {
+
+              var dockIcons = $('.list').clone().find('i').attr('id','menu').addClass('menu-icon large');
+            }
+            $('.icon-wrapper').html(dockIcons);
+            $('.icon-line').hide();
+            // $('.icon-wrapper').prepend('<i id="menu" class="demo-icon menu-icon ">&#xe892;</i>');
           });
       //swipe end
 
     });
 
-
+    //Pridavanie ikoniek
     $(document).on('touchend','.add',function () {
       $('#'+addID).html($(this).clone().removeClass('add'));
         if (addID<4) {
           addID++;
           $('.dlist').css('border', '2px solid transparent');
-          $('#'+addID).css('border', '2px solid red');
+          $('#'+addID).css('border', '2px solid white');
         }
         else{
           addID=1;
           $('.dlist').css('border', '2px solid transparent');
-          $('#'+addID).css('border', '2px solid red');
+          $('#'+addID).css('border', '2px solid white');
         }
     });
   }
